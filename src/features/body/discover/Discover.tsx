062f9bee733a53b75c9e51229961041b30062582
Calvin Dalenta <calvindalentark@gmail.com>
2021-08-31 03:16:17 +0700

    Add trending and trending card component

--- src/features/body/discover/Discover.tsx
+++ src/features/body/discover/Discover.tsx
@@ -4,28 +4,44 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectTrackState } from '../../../slices/trackSlice';
 import BrowseAll from './BrowseAll';
 import MostWatched from './MostWatched';
+import Trending from './Trending';
 
 function Discover(){
     const tracks = useSelector(selectTrackState);
 
     const view = tracks.value.map(track => {
         return <p>{track.author}</p>
-    })    
+    });
 
-    return(
-        <div className="discover">
+    function getView(){
+        if (tracks.value.length === 0){
+            // return loading
+            return <p>Loading...</p>;
+        } else {
+            const result =
             <Grid
             container
             spacing={1}
             style={{backgroundColor: 'lightgreen'}}
             >
-                <Grid item xs={12}>
+                <Grid item xs={12} style={{marginBottom: '3vh'}}>
+                    <Trending/>
+                </Grid>
+                <Grid item xs={12} style={{marginBottom: '3vh'}}>
                     <MostWatched/>
                 </Grid>
-                <Grid item xs={12}>
+                <Grid item xs={12} style={{marginBottom: '3vh'}}>
                     <BrowseAll/>
                 </Grid>
             </Grid>
+            return result;
+
+        }
+    }
+
+    return(
+        <div className="discover">
+            {getView()}
         </div>
     );
 }
